25d051bcf7f5839bc9d51db85ec6e9406a35f68f
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-08-10 16:43:31 +0300

    2.1. Конструкторы.[191430#339469]

---
+++ src/main/kotlin/ru/job4j/oop/Student.kt
@@ -0,0 +1,27 @@
+package ru.job4j.oop
+
+class Student {
+    var name: String = ""
+    var surname: String = ""
+    var phone: String = ""
+    var email: String = ""
+
+    constructor(name : String) {
+        this.name = name
+    }
+
+    constructor(name : String, surname : String) : this(name) {
+        this.surname = surname
+    }
+
+    constructor(name : String, surname : String, phone : String) : this(name) {
+        this.surname = surname
+        this.phone = phone
+    }
+
+    constructor(name : String, surname : String, phone : String, email : String) : this(name) {
+        this.surname = surname
+        this.phone = phone
+        this.email = email
+    }
+}
