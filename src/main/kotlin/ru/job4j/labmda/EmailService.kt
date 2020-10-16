c7c38b5614fbdf3866514a3ce8efab65cf9ee87e
Maxim Razumkov <m.razumkov@vniims.ru>
2020-10-16 15:36:31 +0300

    1. KDoc. Документация.[338927#339501]

--- src/main/kotlin/ru/job4j/labmda/EmailService.kt
+++ src/main/kotlin/ru/job4j/labmda/EmailService.kt
@@ -1,6 +1,17 @@
 package ru.job4j.labmda
 
+/**
+ * Service for sending letters
+ * @author m.razumkov
+ * @since 1.8
+ * @version 1.0.0
+ */
 class EmailService {
+    /**
+     * to create message
+     * @param message
+     * @return string with message
+     */
     fun emailTo(message: Message) = with(StringBuilder()) {
             append("Subject : ").append(message.email)
             append("Body : ").append("Hello, ").append(message.email).append(" ")
